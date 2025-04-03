bb54eba7f4bfcff3274bbee6cd07b35d83fe124e
cds-amal <cds.sudama@gmail.com>
2025-04-02 23:58:32 -0400

    add fugitive and preview md

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -44,5 +44,17 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
     opts = {},
-  }
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  'tpope/vim-fugitive',
 }
+
+-- install with yarn or npm
