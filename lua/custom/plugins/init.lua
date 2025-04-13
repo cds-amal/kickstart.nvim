dcbf6308a47ea3a6b14ec244f4cdd8fcacd2e053
cds-amal <cds.sudama@gmail.com>
2025-04-12 20:39:39 -0400

    Add zellij-nav

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -114,6 +114,18 @@ return {
       { '<leader>gt', '<cmd>GitBlameToggle<cr>', desc = '[G]it [T]oggle Blame' },
     },
   },
+  {
+    'swaits/zellij-nav.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    keys = {
+      { '<c-h>', '<cmd>ZellijNavigateLeftTab<cr>', { silent = true, desc = 'navigate left or tab' } },
+      { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+      { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+      { '<c-l>', '<cmd>ZellijNavigateRightTab<cr>', { silent = true, desc = 'navigate right or tab' } },
+    },
+    opts = {},
+  },
 }
 
 -- install with yarn or npm
