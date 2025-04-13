d18b3a3c712b87b8a5078c1e2c46062b6299e85f
cds-amal <cds.sudama@gmail.com>
2025-04-12 20:38:03 -0400

    Switch to lazygit remove fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -54,7 +54,28 @@ return {
     end,
     ft = { 'markdown' },
   },
-  'tpope/vim-fugitive',
+  --- nvim v0.8.0
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+  --'tpope/vim-fugitive',
 }
 
 -- install with yarn or npm
