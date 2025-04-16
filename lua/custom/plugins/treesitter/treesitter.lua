f436300c7706b77c3926675b5d691d6b1df0f639
cds-amal <cds.sudama@gmail.com>
2025-04-16 13:58:46 -0400

    Add treesitter-context

--- lua/custom/plugins/treesitter/treesitter.lua
+++ lua/custom/plugins/treesitter/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'nvim-treesitter/nvim-treesitter-context' },
   },
 
   'nvim-treesitter/nvim-treesitter-textobjects',
