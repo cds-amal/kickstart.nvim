5e0b068711d4874b57714dd0a76edd6dfd0d712b
cds-amal <cds.sudama@gmail.com>
2025-04-17 23:30:56 -0400

    wip: Update kulala

--- lua/custom/plugins/kulala.lua
+++ lua/custom/plugins/kulala.lua
@@ -1,13 +1,13 @@
 return {
   'mistweaverco/kulala.nvim',
   keys = {
-    { '<leader>Rs', desc = 'Send request' },
-    { '<leader>Ra', desc = 'Send all requests' },
-    { '<leader>Rb', desc = 'Open scratchpad' },
+    { '<leader>rs', desc = 'Send request' },
+    { '<leader>ra', desc = 'Send all requests' },
+    { '<leader>rb', desc = 'Open scratchpad' },
   },
   ft = { 'http', 'rest' },
   opts = {
     -- your configuration comes here
-    global_keymaps = false,
+    global_keymaps = true,
   },
 }
