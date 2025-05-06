01c56ae95ef5a0b5257dffb0fb83897b4b345953
cds-amal <cds.sudama@gmail.com>
2025-05-06 10:53:03 -0400

    Add CueVet

---
+++ lua/custom/plugins/cue.lua
@@ -0,0 +1,8 @@
+return {
+  dir = vim.fn.stdpath 'config' .. '/lua/custom/cue_local',
+  name = 'cue_local',
+  ft = 'cue',
+  config = function()
+    require('custom.cue_local').setup()
+  end,
+}
