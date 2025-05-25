98b69771c7b0ff9d9ba94a6efed3b9de24a6a28f
cds-amal <cds.sudama@gmail.com>
2025-05-24 22:39:36 -0400

    Add jqx

--- init.lua
+++ init.lua
@@ -1234,6 +1234,7 @@ require('lazy').setup({
 require 'custom.plugins.customgit.commit'
 require 'custom.plugins.utils.messages'
 require 'custom.keymaps'
+require 'custom.abi-tools'
 require 'custom.close-unnamed'
 require 'custom.strip-whitespaces'
 
