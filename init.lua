157d8642bd9d4637ff4a5355c425e08ffd531a84
cds-amal <cds.sudama@gmail.com>
2025-05-24 22:38:29 -0400

    Add strip-whitespaces

--- init.lua
+++ init.lua
@@ -1234,6 +1234,7 @@ require('lazy').setup({
 require 'custom.plugins.customgit.commit'
 require 'custom.plugins.utils.messages'
 require 'custom.keymaps'
+require 'custom.strip-whitespaces'
 
 _G.dd = function(...)
   require('snacks').debug.inspect(...)
