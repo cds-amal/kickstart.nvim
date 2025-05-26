dfb65f5b1b0db0261e9ab3420d27b8cd12f42d38
cds-amal <cds.sudama@gmail.com>
2025-05-26 11:52:28 -0400

    Update hexer keys

--- lua/custom/plugins/hexer.lua
+++ lua/custom/plugins/hexer.lua
@@ -1,10 +1,13 @@
 return {
-  'cds-amal/hexer-nvim',
+  -- 'cds-amal/hexer-nvim',
+  dir = '~/dev/nvim-plugins/hexer-nvim',
+  dev = true,
   name = 'hexer',
   lazy = false,
   config = function()
     local hex = require 'hexer'
-    vim.keymap.set('n', '<leader>hh', hex.format_calldata, { desc = 'Expand Calldata' })
-    vim.keymap.set('n', '<leader>ha', hex.format_calldata, { desc = 'Convert bytes to ascii' })
+    vim.keymap.set('n', '<leader>hf', hex.format_calldata, { desc = '[H]exer [F]ormat Calldata' })
+    vim.keymap.set('n', '<leader>ha', hex.bytes_to_ascii, { desc = '[H]exer bytes to [A]scii' })
+    vim.keymap.set('n', '<leader>hB', hex.abi_decode, { desc = '[H]exer decode to A[B]I' })
   end,
 }
