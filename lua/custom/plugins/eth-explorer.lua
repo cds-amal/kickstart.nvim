34b528329f13a1f9da019487ff68f022125c4b1c
cds-amal <cds.sudama@gmail.com>
2025-05-30 22:57:26 -0400

    remove eth-explorer experiment

--- lua/custom/plugins/eth-explorer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    dir = vim.fn.stdpath 'config' .. '/lua/custom/plugins/eth-explorer',
-    name = 'ethereum-explorer',
-    config = function()
-      require('custom.plugins.eth-explorer.eth-explorer').setup()
-    end,
-  },
-}
