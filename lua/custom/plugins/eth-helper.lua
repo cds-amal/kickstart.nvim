162f86b06187cef911fe38cceb7ee486b6862130
cds-amal <cds.sudama@gmail.com>
2025-05-30 22:12:22 -0400

    Update eth-helper

--- lua/custom/plugins/eth-helper.lua
+++ lua/custom/plugins/eth-helper.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   config = function()
     require('eth-helper').setup {
+      scan_on = {},
       -- addresses = {
       --   ['0x093Ddc1DD7784029b32d44F0C11B29AEb8232cc9'] = 'Heidi',
       --   ['0x7aBF46564cfd4d67E36DC8fB5DeF6a1162EBaF6b'] = 'Grace',
