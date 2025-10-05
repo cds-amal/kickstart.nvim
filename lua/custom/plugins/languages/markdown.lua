232cc5b389e15b5e9ee012c5d2876df35b45377d
cds-amal <cds.sudama@gmail.com>
2025-10-05 08:17:05 -0400

    wip: cleanup

--- lua/custom/plugins/languages/markdown.lua
+++ lua/custom/plugins/languages/markdown.lua
@@ -1,16 +1,22 @@
 -- selene:allow(mixed_table)
+
 return {
-  -- {
-  --   'MeanderingProgrammer/render-markdown.nvim',
-  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-  --   opts = {},
-  -- },
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && yarn install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+
+      -- Define the Vimscript function directly
+      vim.cmd [[
+        function! OpenMarkdownPreview(url)
+          echom "URL received: " . a:url
+          execute "silent ! open -a 'Zen Browser' -n --args --new-window " . a:url
+        endfunction
+]]
+
+      vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
     end,
     ft = { 'markdown' },
   },
