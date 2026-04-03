507d291913eab9deddd1063235fe235bd0b748a9
cds-amal <cds.sudama@gmail.com>
2026-04-02 20:08:46 -0400

    fix(markdown): wrap plugin spec in table and move keymaps into config
    The markdown-preview spec was returned as a bare table rather than
    wrapped in a list, which can cause issues with lazy.nvim's spec
    merging. This wraps it properly and moves the keymaps from the `keys`
    field into the `config` function as explicit `vim.keymap.set` calls
    (so they're only registered once the plugin is actually loaded).

--- lua/custom/plugins/languages/markdown.lua
+++ lua/custom/plugins/languages/markdown.lua
@@ -1,18 +1,18 @@
 -- selene:allow(mixed_table)
 
 return {
-  'selimacerbas/markdown-preview.nvim',
-  dependencies = { 'selimacerbas/live-server.nvim' },
-  cmd = { 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  config = function()
-    require('markdown_preview').setup {
-      open_browser = true,
-      mermaid_renderer = 'rust',
-    }
-  end,
-  keys = {
-    { '<leader>mp', '<cmd>MarkdownPreview<cr>', desc = 'Markdown Preview Start', ft = 'markdown' },
-    { '<leader>ms', '<cmd>MarkdownPreviewStop<cr>', desc = 'Markdown Preview Stop', ft = 'markdown' },
+  {
+    'selimacerbas/markdown-preview.nvim',
+    dependencies = { 'selimacerbas/live-server.nvim' },
+    cmd = { 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    config = function()
+      require('markdown_preview').setup {
+        open_browser = true,
+        mermaid_renderer = 'rust',
+      }
+      vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<cr>', { desc = 'Markdown Preview Start' })
+      vim.keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop<cr>', { desc = 'Markdown Preview Stop' })
+    end,
   },
 }
